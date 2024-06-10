699948c25b5f552a351e65256ddae6fc7cb1a5d4
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-06-10 04:35:32 +0100

    minor fix

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -1,7 +1,7 @@
 const Video = () => {
   return (
     <div className=" px-4 py-8 md:px-12 text-[#131313CC] flex flex-col md:flex-row gap-12">
-      <div className=" bg-emerald-600 w-full md:basis-[60%]">
+      <div className="  w-full md:basis-[60%]">
         <video controls width="100%">
           <source src="/path/to/placeholder-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
